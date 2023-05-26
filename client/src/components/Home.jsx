d0cbd2f4ac41efe4a349beb74d614b4ba609387f
Rishabh Kumar <rishabh.kumar10101@icloud.com>
2023-05-26 23:30:14 +0530

    adding files and folder in smart_contract folder

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -2,29 +2,39 @@ import React from 'react'
 // import { AiFillPayCircle } from 'react-icons/ai';
 import { BsInfoCircle } from 'react-icons/bs';
 import { SiEthereum } from 'react-icons/si';
+import Loader from './Loader';
 
 // import { Loader } from './'
 
 const Home = () => {
   const wallet = () => {
+
   }
+
+  const handleSubmit = () => {
+
+  }
+
   const commonStyle = "min-h-[70px] sm:px-0 px-2 sm:min-w-[120px] flex justify-center items-center border-[0.5px] border-gray-400 text-sm font-light text-white"
 
-  const Input = (placeholder) => (
+  const Input = (placeholder, name, type, value, handleChange) => (
     <Input
-    placeholder = {placeholder}
-
+      placeholder={placeholder}
+      type={type}
+      step="0.0001"
+      value={value}
+      onChnge={(e)=> handleChange(e, name)}
+      className="my-2 w-full rounded-sm p-2 outline-none bg-transparent text-white border-none text-sm white-glassmorphism"
     />
   );
   return (
     <div className='flex w-full justify-center items-center'>
-    <h1>home page</h1>
-      <div className='flex md:flex-row flex-col items-start justify-between md:p-20 py-12 px-4'>
-        <div className='flex flex-1 justify-start flex-col md:mr-10'>
-          <h1 className='text-3xl md:text-5xl text-white text-gradient py-1'>
+      <div className='flex mf:flex-row flex-col items-start justify-between :p-20 py-12 px-4'>
+        <div className='flex flex-1 justify-start flex-col mf:mr-10'>
+          <h1 className='text-3xl :text-5xl text-white text-gradient py-1'>
             Send Crypto <br /> accross the world
           </h1>
-          <p className='text-left mt-5 text-white font-light md:w-9/12 w-11/12 text-base'>
+          <p className='text-left mt-5 text-white font-light :w-9/12 w-11/12 text-base'>
             Explore... Buy and sell crypto
           </p>
           <button
@@ -34,7 +44,7 @@ const Home = () => {
             <p className='text-white text-base font-semibold'>connect Wallet</p>
           </button>
           {/* making a grid for display features */}
-          <div className='grid grid-col-2 md:grid-col-3 w-full mt-10'>
+          <div className='grid grid-col-2 :grid-col-3 w-full mt-10'>
             <div className={`rounded-tl-2xl ${commonStyle}`}>
               Reliability
             </div>
@@ -56,16 +66,16 @@ const Home = () => {
           </div>
         </div>
         {/* card component */}
-        <div className='flex flex-col flex-1 items-center justify-start w-full md:mt-0 mt-10'>
+        <div className='flex flex-col flex-1 items-center justify-start w-full mf:mt-0 mt-10'>
           <div className='p-3 justify-end items-start flex-col rounded-xl h-40 sm:w-72 w-full my-5 eth-card white-glassmorpism'>
             <div className='felx justify-between flex-col w-full h-full'>
-                {/* logo Div which is circle in the card on top right */}            
+              {/* logo Div which is circle in the card on top right */}
               <div className='flex justify-between items-start'>
                 {/* logo Div which is circle in the card on top left */}
                 <div className='w-10 h-10 rounded-full border-2 border-white flex justify-center items-center'>
                   <SiEthereum fontSize={21} color='#fff' />
                 </div>
-                <BsInfoCircle fontSize={17} color='#fff'/>
+                <BsInfoCircle fontSize={17} color='#fff' />
               </div>
               <div className=''>
                 <p className='text-white font-light text-sm mt-10'>
@@ -73,13 +83,31 @@ const Home = () => {
                 </p>
                 <p className='text-white font-semibold text-lg mt-1'>
                   Ethereum
-                </p>                
+                </p>
               </div>
             </div>
           </div>
           {/* form div */}
           <div className='p-5 dm:w-96 w-full flex flex-col justify-start items-center blue-glassmorphism'>
-            <Input placeholder="address To " name="addressTo" type="text" handleChange={() => {}}/>
+            <Input placeholder="Address To " name="addressTo" type="text" handleChange={() => { }} />
+            <Input placeholder="Amount (ETH) " name="amount" type="number" handleChange={() => { }} />
+            <Input placeholder="Keyword (Gif) " name="keyword" type="text" handleChange={() => { }} />
+            <Input placeholder="Enter Message" name="message" type="text" handleChange={() => { }} />
+            
+            {/* self closing div for line under input field */}
+            <div className='h-[1px] w-full bg-gray-400 my-2'/>
+
+            {flase ? (
+                  <Loader />
+            ): (
+              <button
+              type="button"
+              onClick={handleSubmit}
+              className='text-white w-full mt-2 border-[1px] p-2 border-[#3d4f7c] rounded-full cursor-pointer'
+              >
+                Send Now
+              </button>
+            )}
           </div>
         </div>
       </div>
---
+++ smart_contract/contracts/transactions.sol
@@ -0,0 +1,8 @@
+// soloidity is a combination of javaScript , java , c++ , Rust and other languages
+//SPDX-License-Identifire: UNLICENSED
+
+pragma solidity ^0.8.0;
+
+contract Transactions {
+                                                                                    
+}
